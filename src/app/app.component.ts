df74bbffbd2bb0a4dcfcb534cd1cdba5342434ec
Ale Mela <alessiamelampo4@gmail.com>
2025-05-21 14:56:20 +0200

    modifiche titolo, lista prodotti, card prodotto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,17 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { ListaProdottiComponent } from "./components/lista-prodotti/lista-prodotti.component";
+import { TitleComponent } from './components/title/title.component';
 
 //E' UN DECORATORE CHE AGGIUNGE DELLE FUNZIONALITA' ALLA CLASSE SENZA ANDARLA A MODIFICARE DIRETTAMENTE
 @Component({
   selector: 'app-root', //è il tag che troviamo in html e che utilizziamo per richiamare il componente
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, ListaProdottiComponent], 
+  imports: [RouterOutlet, 
+    //HeaderComponent, 
+    FooterComponent, 
+    ListaProdottiComponent,
+    TitleComponent
+  ], 
   templateUrl: './app.component.html', //è il file html che andremo a utilizzare per il componente
   styleUrl: './app.component.css' //è il file css che andremo a utilizzare per il componente
 })
